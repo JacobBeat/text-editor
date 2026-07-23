c676b78bcb4274f55623dada7f6fbccfd41643be
Jacob Beatty <jacobbeatty1023@gmail.com>
2026-07-22 21:48:47 -0400

    Write from terminal into text file

--- main.cpp
+++ main.cpp
@@ -1,3 +1,20 @@
+#include <fstream>
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main() {}
+using namespace std;
+
+void write_into_file(ofstream &out, string &data) { out << data; }
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    cout << "Use one argument.\n";
+    return 1;
+  }
+  ofstream ost{argv[1]};
+  string buffer;
+  while (cin >> buffer) {
+    write_into_file(ost, buffer);
+  }
+}
