f4d124cc7a9f291f6328cdefcd5c717b96a24307
Jacob Beatty <jacobbeatty1023@gmail.com>
2026-07-24 20:40:39 -0400

    Class for handling text file

--- main.cpp
+++ main.cpp
@@ -5,16 +5,36 @@
 
 using namespace std;
 
-void write_into_file(ofstream &out, string &data) { out << data; }
+class TextFile {
+private:
+  vector<string> data; // each element of data should be a line in the file
+public:
+  void read(ifstream &fin) {
+    for (string s; fin >> s;) {
+      data.push_back(s);
+    }
+  }
+  void write(ofstream &fout) {
+    for (string s : data) {
+      fout << s;
+    }
+  }
+  TextFile() : data() {};
+};
 
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     cout << "Use one argument.\n";
     return 1;
   }
+  vector<string> infile;
+  ifstream ist{argv[1]};
+  for (string s; ist >> s;) {
+    infile.push_back(s);
+  }
   ofstream ost{argv[1]};
-  string buffer;
-  while (cin >> buffer) {
-    write_into_file(ost, buffer);
+  char c;
+  while (cin.get(c)) {
+    ost << c;
   }
 }
