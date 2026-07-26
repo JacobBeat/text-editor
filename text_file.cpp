bdb40bea7fce9d48b6af8cb5962d217d7ab79918
Jacob Beatty <jacobbeatty1023@gmail.com>
2026-07-25 21:32:16 -0400

    Working line editor

--- text_file.cpp
+++ text_file.cpp
@@ -1,11 +1,11 @@
 #include "text_file.hpp"
 #include <iostream>
-#include <stdexcept>
 
 using namespace std;
 
 void TextFile::read(ifstream &fin) { // Loads each line of fin into data
-  for (string s; fin >> s;) {
+  string s;
+  while (getline(fin, s)) {
     data.push_back(s);
   }
 }
@@ -17,28 +17,51 @@ void TextFile::write(ofstream &fout) { // Writes out data to fout
 void TextFile::replace(
     const string &output,
     int number) { // replaces line # number with string output
+  number -= 1;    // Use 1-based indexing
+  if (number < 0) {
+    cout << "Line number must be greater than or equal to one\n";
+    return;
+  }
   if (number >= size(data)) {
-    throw invalid_argument("Attempted to access outside of file");
+    for (int i = size(data); i < number; i++) {
+      data.push_back("");
+    }
+    data.push_back(output);
+    return;
   }
   data[number] = output;
 }
 void TextFile::append(
     const string &output,
     int number) { // adds string output to end of line # number
-  if (number >= size(data)) {
-    throw invalid_argument("Attempted to access outside of file");
+  number -= 1;
+  if (number < 0) {
+    cout << "Line number must be greater than or equal to one\n";
+    return;
   }
-  data[number] += (" " + output);
+  if (number >= size(data)) {
+    for (int i = size(data); i < number; i++) {
+      data.push_back("");
+    }
+    data.push_back(output);
+    return;
+  }
+  data[number] += output;
 }
 void TextFile::printline() {
   for (int line = 0; line < size(data); line++) {
-    cout << "Line " << line << ": " << data[line] << "\n";
+    cout << "Line " << line + 1 << ": " << data[line] << "\n";
   }
 }
 void TextFile::printline(int line) {
-  if (line >= size(data)) {
-    throw invalid_argument("Attempted to access outside of file");
+  if (line < 1) {
+    cout << "Line number must be greater than or equal to one\n";
+    return;
   }
-  cout << "Line: " << line << "\n" << data[line] << "\n";
+  if (line > size(data)) {
+    cout << "Attempting to print outside of file";
+    return;
+  }
+  cout << "Line: " << line << "\n" << data[line - 1] << "\n";
 }
 TextFile::TextFile() : data() {};
